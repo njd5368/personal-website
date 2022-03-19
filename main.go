86b56864e7dd0d16de795d7d2d6911a7f767275c
marauder586 <njd5368@rit.edu>
2022-03-19 14:44:43 -0400

    added favicon.ico

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/gotailwindcss/tailwind/twembed"
+	"github.com/gotailwindcss/tailwind/twhandler"
 	"html/template"
 	"log"
 	"net/http"
@@ -15,6 +17,10 @@ func init() {
 	tpl = template.Must(template.ParseGlob("site/templates/*"))
 }
 
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "site/images/favicon.ico")
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	err := tpl.ExecuteTemplate(w, "index.html", nil)
 
@@ -25,9 +31,11 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
+	r.Handle("/site/css/{css-file}", twhandler.New(http.Dir("./site/css"), "/site/css", twembed.New()))
 	r.Handle("/site/images/{image}", http.StripPrefix("/site/images", http.FileServer(http.Dir("./site/images"))))
+	r.HandleFunc("/favicon.ico", faviconHandler)
 	r.HandleFunc("/", indexHandler)
 
-	log.Println("Starting server...")
+	log.Println("Starting server....")
 	log.Fatal(http.ListenAndServe(addr, r))
 }
