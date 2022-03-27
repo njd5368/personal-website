0a9b4acda16f79a1a284f1a0fbb51265ae514c9c
marauder586 <njd5368@rit.edu>
2022-03-27 11:57:29 -0400

    Adjusted tailwind further

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./tailwindcss -i site/css/tailwind-input.css -o site/css/main.css
+go run main.go
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+      './site/templates/**/*.gohtml'
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#000',
+      'white': '#fff',
+      'gray': {
+        100: '#D9E0EE',
+        200: '#C3BAC6',
+        300: '#988BA2',
+        400: '#6E6C7E',
+        500: '#575268',
+        600: '#302D41',
+        700: '#1E1E2E',
+        800: '#1A1826',
+        900: '#161320',
+      },
+      'flamingo': '#F2CDCD',
+      'mauve':    '#DDB6F2',
+      'pink':     '#F5C2E7',
+      'maroon':   '#E8A2AF',
+      'red':      '#F28FAD',
+      'peach':    '#F8BD96',
+      'yellow':   '#FAE3B0',
+      'green':    '#ABE9B3',
+      'teal':     '#B5E8E0',
+      'blue':     '#96CDFB',
+      'sky':      '#89DCEB',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
